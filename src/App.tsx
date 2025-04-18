87dac76968006db256d50d198df7798e1549b68e
Isaac Delgado <isaacdelgado1994@gmail.com>
2025-04-17 20:14:52 -0700

    hey

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import TodoItem from "./components/Todoitem";
+import { dummyData } from "./data/todo";
+import AddTodoForm from "./components/AddTodoForm";
+
+function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+  }
+
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title: title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
+  return (
+    <main className="py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <div className="space-y-2">
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              onCompletedChange={setTodoCompleted}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data/todo.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "LIke this video",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Subscribe to Codign in Flow",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a nice comment",
+    completed: true,
+  },
+];
